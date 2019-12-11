8f975b31f0b4cef8f80067882d73608872ac1e8e
lpiem <lpiem@iutabgdin202-02.univ-lyon1.fr>
2019-12-11 17:10:20 +0100

    Add Datasource Selection

--- app/src/main/java/fr/appsolute/tp/ui/fragment/CharacterDetailFragement.kt
+++ app/src/main/java/fr/appsolute/tp/ui/fragment/CharacterDetailFragement.kt
@@ -1,27 +1,34 @@
 package fr.appsolute.tp.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
 import com.bumptech.glide.Glide
 import fr.appsolute.tp.R
+import fr.appsolute.tp.data.model.Episode
 import fr.appsolute.tp.ui.activity.MainActivity
 import fr.appsolute.tp.ui.viewmodel.CharacterViewModel
+import fr.appsolute.tp.ui.viewmodel.EpisodeViewModel
+import kotlinx.android.synthetic.main.fragment_character_detail.*
 import kotlinx.android.synthetic.main.fragment_character_detail.view.*
 
 class CharacterDetailFragment : Fragment() {
 
     private lateinit var characterViewModel: CharacterViewModel
+    private lateinit var episodeViewModel: EpisodeViewModel
     private var characterId = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activity?.run {
             characterViewModel = ViewModelProvider(this, CharacterViewModel).get()
+            episodeViewModel = ViewModelProvider(this, EpisodeViewModel).get()
         } ?: throw IllegalStateException("Invalid Activity")
         characterId =
             arguments?.getInt(ARG_CHARACTER_ID_KEY) ?: throw IllegalStateException("No ID found")
@@ -50,6 +57,12 @@ class CharacterDetailFragment : Fragment() {
                 Glide.with(this)
                     .load(it.image)
                     .into(this.imageView)
+            }
+        }
+
+        btnListEpisode.setOnClickListener {
+            var listEpisode = episodeViewModel.listEpisode {
+                Toast.makeText(this.activity, it?.get(10)?.name, Toast.LENGTH_SHORT).show()
             }
         }
     }
--- app/src/main/java/fr/appsolute/tp/ui/viewmodel/EpisodeViewModel.kt
+++ app/src/main/java/fr/appsolute/tp/ui/viewmodel/EpisodeViewModel.kt
@@ -1,9 +1,27 @@
 package fr.appsolute.tp.ui.viewmodel
 
-import androidx.lifecycle.AndroidViewModel
-import fr.appsolute.tp.RickAndMortyApplication
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import fr.appsolute.tp.data.model.Episode
 import fr.appsolute.tp.data.repository.EpisodeRepository
+import kotlinx.coroutines.launch
 
-class EpisodeViewModel(application: RickAndMortyApplication): AndroidViewModel(application) {
-    val repository = EpisodeRepository.newInstance(application)
+typealias OnGetResult<T> = (List<Episode>?) -> Unit
+class EpisodeViewModel(
+    private val repository: EpisodeRepository
+): ViewModel() {
+
+    fun listEpisode(onGetResult: OnGetResult<Episode?>){
+        viewModelScope.launch {
+            repository.getAllEpisode()?.run(onGetResult)
+        }
+    }
+
+    companion object Factory : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return EpisodeViewModel(EpisodeRepository.instance()) as T
+        }
+    }
 }
